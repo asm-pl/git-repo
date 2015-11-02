e9742a48a009d42efe5ebee50707dace7b465e0e
asm-pl <vs.sharp@yandex.ru>
2015-11-03 00:50:22 +0300

    bhv

---
+++ Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Item
+    {
+        public string Name { get; set; }
+
+        public Item(){ }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
