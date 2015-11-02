29fb2268b9cdf7fc458b6b50f01c36b7ebc7b4c1
asm-pl <vs.sharp@yandex.ru>
2015-11-03 00:21:00 +0300

    xggjhn

--- test/Form1.cs
+++ test/Form1.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApplication35
 {
     public partial class Form1 : Form
     {
+        private int i = 0;
         public Form1()
         {
             InitializeComponent();
